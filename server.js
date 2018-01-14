a7ba080ed3e5060b2711a92f1d4fbe682273e850
aarav kumar <aaravkumar96.ak@gmail.com>
2018-01-14 19:09:02 +0530

    initial message

---
+++ server.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// hbs helper
+hbs.registerHelper('getCurrentyear', ()=>{
+return new Date().getFullYear();   
+    
+});
+
+// middleware
+app.use((req,res,next) =>{
+var now = new Date().toString();
+ var log =`${now}: ${req.method} ${req.url}`   
+console.log(log);
+    fs.appendFile('server.log',log + '\n',(err)=>{
+      if(err){
+          console.log("error");
+      }  
+    });
+next();
+    
+});
+
+
+
+//app.use((req,res,next) =>{
+//   res.render('maintainance.hbs');
+//});
+
+app.get('/',(req,res) => { 
+  res.render('home.hbs',{
+     pagetitle :' Welcome Handlebar',
+      welcome : ' Hello i am learning handlebar js and express and i am loving it.'
+     
+  });
+   
+});
+
+
+app.get('/about',(req,res) =>{
+   res.render('about.hbs',{
+      pagetitle : 'hello handle bar',
+     
+   });
+});
+
+app.get('/bad', (req,res) =>{
+    res.send({
+        error:'unable to process your request'
+    });
+});
+
+
+app.listen(3000 , () => {
+    console.log('server is up on port 3000');
+});
+
